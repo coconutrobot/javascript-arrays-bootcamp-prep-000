5deca38a2bbd1b695f4a8ebc8468ce2502397dbe
Kacia Hosmer <kacia@coconutrobot.com>
2018-05-18 19:07:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,7 +35,7 @@ function destructivelyRemoveElementFromBeginningOfArray (chocolateBars) {
 
 function removeElementFromBeginningOfArray (chocolateBars) {
   
-  chocolateBars.slice(1);
+  chocolateBars = chocolateBars.slice(1);
 }
 
 function destructivelyRemoveElementFromEndOfArray (chocolateBars) {
